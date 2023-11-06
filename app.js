edd06908955c0dd9053f1ed405f3fb1be7d3641a
Vamshix57 <vamshi.krishna@kalvium.community>
2023-11-06 12:44:54 +0530

    Done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var startBtn = document.getElementById("start-btn")
 var instructionBtn = document.getElementById("instruction-btn")
+
 startBtn.addEventListener('click', (e) =>{
-    window.location.href = "./game.html";
+    window.location.href = "./user.html";
 })
 
 instructionBtn.addEventListener('click', (e)=>{
